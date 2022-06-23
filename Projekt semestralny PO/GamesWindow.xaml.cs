79c37fc5519d863e36327ccf15a1c376d31d1f67
AnnaBala-WSEI <anna.bala@microsoft.wsei.edu.pl>
2022-06-23 19:32:04 +0200

    Add new window with the list of video games

---
+++ Projekt semestralny PO/GamesWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt_semestralny_PO
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GamesWindow.xaml
+    /// </summary>
+    public partial class GamesWindow : Window
+    {
+        public GamesWindow()
+        {
+            InitializeComponent();
+
+            VideoGamesPortalEntities db = new VideoGamesPortalEntities();
+            
+            var games = from game in db.games select new {
+                ID = game.game_id,
+                Title = game.game_title,
+                ReleaseYear = game.release_year,
+                IsASerie =  game.is_a_serie,
+            };
+
+            this.gridVideoGames.ItemsSource = games.ToList();
+        }
+    }
+}
